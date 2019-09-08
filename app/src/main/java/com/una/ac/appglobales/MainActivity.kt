9630144e58a1e3a05609475263ab8c24653ba729
rafaelchs <rafaelchevez4@gmail.com>
2019-09-07 19:27:57 -0600

    Initial

---
+++ app/src/main/java/com/una/ac/appglobales/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.una.ac.appglobales
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
