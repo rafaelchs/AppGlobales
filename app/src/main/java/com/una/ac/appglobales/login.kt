de3c3ad39993703018b51d46091ab2a2473eb93b
rafaelchs <rafaelchevez4@gmail.com>
2019-09-13 15:46:31 -0600

    colores agregados

--- app/src/main/java/com/una/ac/appglobales/login.kt
+++ app/src/main/java/com/una/ac/appglobales/login.kt
@@ -3,7 +3,7 @@ package com.una.ac.appglobales
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
